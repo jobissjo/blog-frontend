037d69abe4a9292aab191e013c85b457c398f259
Jobi S S <jobisjobi1234@gmail.com>
2026-02-17 20:58:37 +0530

    seo

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://jo-tech-blog.vercel.app";
+
+  const res = await fetch(
+    "https://blog-fastapi-drab.vercel.app/api/blog"
+  );
+
+  const data = await res.json();
+
+  const blogs = data.data;
+
+  const blogUrls = blogs.map((blog: any) => ({
+    url: `${baseUrl}/blog/${blog.slug}`,
+    lastModified: new Date(blog.updated_at),
+    changeFrequency: "monthly" as const,
+    priority: 0.8,
+  }));
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...blogUrls,
+  ];
+}
