d7f92a97973de71f77d342b10caee6d4f64d73b8
Jobi S S <jobisjobi1234@gmail.com>
2026-02-17 22:47:39 +0530

    update series sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,6 +18,29 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         priority: 0.8,
     }));
 
+    const seriesUrl = "https://jotechblog.netlify.app/series"
+    let seriesUrls = [
+        {
+            url: seriesUrl,
+            lastModified: new Date(),
+            changeFrequency: "monthly" as const,
+            priority: 0.8,
+        },
+    ]
+
+    const seriesRes = await fetch(
+        "https://blog-fastapi-drab.vercel.app/api/series/"
+    );
+    const seriesData = await seriesRes.json();
+    const series = seriesData.data;
+    const seriesAllUrls = series.map((series: any) => ({
+        url: `${baseUrl}/series/${series.slug}`,
+        lastModified: new Date(series.updated_at),
+        changeFrequency: "monthly" as const,
+        priority: 0.8,
+    }));
+
+    seriesUrls = [...seriesUrls, ...seriesAllUrls]
     return [
         {
             url: baseUrl,
@@ -26,5 +49,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             priority: 1,
         },
         ...blogUrls,
+        ...seriesUrls,
     ];
 }
